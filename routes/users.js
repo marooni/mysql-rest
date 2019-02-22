aaeb1b167d57da086ea6f60d9f45936a84d5e591
wink_ma <mario.winkler@dlr.de>
2019-02-22 18:08:44 +0100

    share db connection between modules

--- routes/users.js
+++ routes/users.js
@@ -1,4 +1,7 @@
+
 var express = require('express');
+var db = require('../db');
+
 var router = express.Router();
 
 /* GET users listing. */
@@ -8,12 +11,12 @@ var router = express.Router();
 
 
 router.get('/', function(req, res, next) {
-	global.connection.query('select * from arc_archived_ofl', function (error, results, fields) {
+	db.query('select * from arc_archived_ofl where product_type = ''S5P.TROPOMI.L1B''', function (error, results, fields) {
 	  	if(error){
 	  		res.send(JSON.stringify({"status": 500, "error": error, "response": null})); 
 	  		//If there is error, we send the error in the error section with 500 status
 	  	} else {
-  			res.send(JSON.stringify({"status": 200, "error": null, "response": results}));
+  			res.send(JSON.stringify({"status": 200, "error": null, "response": results}), null, 2);
   			//If there is no error, all is good and response is 200OK.
 	  	}
   	});
