57205f30bf9ae73c58645385ebddb8d680bc95c9
marooni <mariowinkler@mail.de>
2019-02-24 09:54:40 +0100

    proper escape of quotes in sql string

--- routes/users.js
+++ routes/users.js
@@ -11,12 +11,12 @@ var router = express.Router();
 
 
 router.get('/', function(req, res, next) {
-	db.query('select * from arc_archived_ofl where product_type = ''S5P.TROPOMI.L1B''', function (error, results, fields) {
+	db.query('select * from arc_archived_ofl where product_type = \'S5P.TROPOMI.L1B\'', function (error, results, fields) {
 	  	if(error){
 	  		res.send(JSON.stringify({"status": 500, "error": error, "response": null})); 
 	  		//If there is error, we send the error in the error section with 500 status
 	  	} else {
-  			res.send(JSON.stringify({"status": 200, "error": null, "response": results}), null, 2);
+  			res.send(JSON.stringify(results, null, 4));
   			//If there is no error, all is good and response is 200OK.
 	  	}
   	});
